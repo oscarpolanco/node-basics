90c254b3e6ea3b446fd98d56d309f0d8e1148d51
Oscar Polanco <opolanco@intellisys.com.do>
2021-07-05 21:46:48 -0400

    Create a util file and require it content from the app file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,5 @@
-const fs = require('fs');
+require('./utils');
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+const name = 'Test'
 
-// Challenge: Append a message to notes.txt
-//
-// 1. Use appendFileSync to append to the file
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text
-
-fs.appendFileSync('notes.txt', ' This is the appended line');
+console.log(name);
