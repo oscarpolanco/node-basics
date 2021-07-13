a7d2d78171f7b92efe81ccdd23917fe1668b3097
Oscar Polanco <opolanco@intellisys.com.do>
2021-07-12 20:48:40 -0400

    Add functionally to the add command creating the loadNotes function; saveNotes function and the addNotes function

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,12 +1,36 @@
-// Challenge: Define and use a function in a new file
-//
-// 1. Create a new file called note.js
-// 2. Create a getNotes function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From app.js, load in and call the function printing message to console
+const fs = require('fs');
 
 function getNotes() {
     return "Your notes...";
 }
 
-module.exports = getNotes;
+const addNote = function(title, body) {
+    const notes = loadNotes();
+    
+    notes.push({
+        title: title,
+        body: body
+    });
+
+    saveNotes(notes);
+}
+
+const saveNotes = function(notes) {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch(e) {
+        return [];
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+};
