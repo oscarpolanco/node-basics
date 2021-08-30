bd511f190c0b052422f38cd5b6b8e61f3533be1a
Oscar Polanco <opolanco@intellisys.com.do>
2021-08-29 22:15:22 -0400

    Use some properties of the weatherstack response

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,6 +3,12 @@ const keys = require('./key');
 
 const url = 'http://api.weatherstack.com/current?access_key=' + keys.weatherstackAPIKey + '&query=37.8267,-122.4233';
 
+// Goal Print a small forecast to the user
+//
+// 1. Print: "Its is currently 9 degrees out. It feels like 5 degrees out."
+// 2. Test your work!
+
 request({ url: url, json: true }, (error, response) =>  {
-    console.log(response.body.current);
+    const currentWeather = response.body.current;
+    console.log('Its is currently ' + currentWeather.temperature + ' degrees out. Its feels like ' + currentWeather.feelslike + ' degrees out.');
 });
