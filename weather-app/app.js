0d63d4c8bbb988da8678b8d101fec81d07b963c0
Oscar Polanco <opolanco@intellisys.com.do>
2021-08-29 21:52:36 -0400

    Ignore the weatherstack api key

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,7 @@
 const request = require('request');
+const keys = require('./key');
 
-const url = 'http://api.weatherstack.com/current?access_key=api_key&query=37.8267,-122.4233';
+const url = 'http://api.weatherstack.com/current?access_key=' + keys.weatherstackAPIKey + '&query=37.8267,-122.4233';
 
 request({ url: url }, (error, response) =>  {
     const data = JSON.parse(response.body);
