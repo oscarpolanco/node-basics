abacbabd31ce48c9fb14a04e7a301990ae39f263
Oscar Polanco <opolanco@intellisys.com.do>
2021-08-29 22:01:04 -0400

    Use request to get a formatted response

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,7 +3,6 @@ const keys = require('./key');
 
 const url = 'http://api.weatherstack.com/current?access_key=' + keys.weatherstackAPIKey + '&query=37.8267,-122.4233';
 
-request({ url: url }, (error, response) =>  {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({ url: url, json: true }, (error, response) =>  {
+    console.log(response.body.current);
 });
